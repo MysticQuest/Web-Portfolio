18a13d88571f5607b8d06105c4e5ce7b0f8f0381
Mystic Quest <l1skou@gmail.com>
2019-11-12 05:04:18 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="wrapper">
+      <div className="App section">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+      <div className="section">
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+      </div>
+      <div className="section">
+        <h1>test2</h1>
+        <h1>test2</h1>
+        <h1>test2</h1>
+        <h1>test2</h1>
+        <h1>test2</h1>
+        <h1>test2</h1>
+        <h1>test2</h1>
+        <h1>test2</h1>
+        <h1>test2</h1>
+        <h1>test2</h1>
+        <h1>test2</h1>
+        <h1>test2</h1>
+        <h1>test2</h1>
+      </div>
     </div>
   );
 }
