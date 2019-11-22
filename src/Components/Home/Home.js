2885df0d64fc7e7f3873c65fdad6e1d940c24e3e
Mystic Quest <l1skou@gmail.com>
2019-11-22 02:31:34 +0200

    replaced css animations with MUI animations

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 //import themes
 import "./Home.css";
 import "../../themes/global.css";
-//import Fade from "@material-ui/core/Fade";
+import Fade from "@material-ui/core/Fade";
 import Slide from "@material-ui/core/Slide";
 //import active section
 import { activeIndex } from "../../App";
@@ -27,52 +27,98 @@ const Home = () => {
             in={activateAnim}
             style={{ transitionDelay: activateAnim ? "0ms" : "0ms" }}
           >
-            <div>
-              <p className="home1">Welcome</p>
-              <p className="home-subhead">
-                to my <span className="highlight">portfolio</span>
-              </p>
-            </div>
+            <p className="home1">Welcome</p>
+          </Slide>
+          <Slide
+            timeout={{ enter: 1200, exit: exitDuration }}
+            direction="right"
+            in={activateAnim}
+            style={{ transitionDelay: activateAnim ? "0ms" : "0ms" }}
+          >
+            <p className="home-subhead">
+              to my <span className="highlight">portfolio</span>
+            </p>
           </Slide>
         </div>
-        <Slide
-          timeout={{ enter: 1500, exit: exitDuration }}
-          direction="right"
-          in={activateAnim}
-          style={{ transitionDelay: activateAnim ? "0ms" : "0ms" }}
-        >
-          <div className="readability-container">
-            <p className="home3">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-              tempor augue risus, ut tempor dolor tincidunt eu. Suspendisse
-              hendrerit lacus enim, a tempus ante sollicitudin eu. Vivamus porta
-              dapibus egestas.
-            </p>
-          </div>
-        </Slide>
-        <Slide
-          timeout={{ enter: 2000, exit: exitDuration }}
-          direction="right"
-          in={activateAnim}
-          style={{ transitionDelay: activateAnim ? "0ms" : "0ms" }}
-        >
-          <p className="home2">Site Contents:</p>
-        </Slide>
-        <Slide
-          timeout={{ enter: 2500, exit: exitDuration }}
-          direction="right"
-          in={activateAnim}
-          style={{ transitionDelay: activateAnim ? "0ms" : "0ms" }}
-        >
-          <div className="readability-container">
-            <ul className="home3">
-              <li>Section 1: Introduction</li>
-              <li>Section 2: Web Projects etc etc etc</li>
-              <li>Section 3: Game Projects with Unity3D</li>
-              <li>Section 4: Information about myself and the site</li>
-            </ul>
-          </div>
-        </Slide>
+        <div className="subtext-container">
+          <Fade
+            timeout={{ enter: 2000, exit: exitDuration }}
+            direction="right"
+            in={activateAnim}
+            style={{ transitionDelay: activateAnim ? "800ms" : "0ms" }}
+          >
+            <div className="readability-container">
+              <p className="home3">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+                tempor augue risus, ut tempor dolor tincidunt eu. Suspendisse
+                hendrerit lacus enim, a tempus ante sollicitudin eu. Vivamus
+                porta dapibus egestas.
+              </p>
+            </div>
+          </Fade>
+          <Slide
+            timeout={{ enter: 2000, exit: exitDuration }}
+            direction="right"
+            in={activateAnim}
+            style={{ transitionDelay: activateAnim ? "3000ms" : "0ms" }}
+          >
+            <p className="home2">Site Contents:</p>
+          </Slide>
+          <Fade
+            timeout={{ enter: 3000, exit: exitDuration }}
+            direction="right"
+            in={activateAnim}
+            style={{ transitionDelay: activateAnim ? "4500ms" : "0ms" }}
+          >
+            <div className="readability-container">
+              <ul className="home3">
+                <Fade
+                  timeout={{ enter: 2000, exit: exitDuration }}
+                  direction="right"
+                  in={activateAnim}
+                  style={{ transitionDelay: activateAnim ? "5000ms" : "0ms" }}
+                >
+                  <li>
+                    <span className="highlight">Section 1</span>: This
+                  </li>
+                </Fade>
+                <Fade
+                  timeout={{ enter: 2000, exit: exitDuration }}
+                  direction="right"
+                  in={activateAnim}
+                  style={{ transitionDelay: activateAnim ? "5500ms" : "0ms" }}
+                >
+                  <li>
+                    <span className="highlight">Section 2</span>: Web Projects
+                    etc etc etc
+                  </li>
+                </Fade>
+                <Fade
+                  timeout={{ enter: 2000, exit: exitDuration }}
+                  direction="right"
+                  in={activateAnim}
+                  style={{ transitionDelay: activateAnim ? "6000ms" : "0ms" }}
+                >
+                  <li>
+                    <span className="highlight">Section 3</span>: Game Projects
+                    with Unity3D
+                  </li>
+                </Fade>
+                <Fade
+                  timeout={{ enter: 2000, exit: exitDuration }}
+                  direction="right"
+                  in={activateAnim}
+                  style={{ transitionDelay: activateAnim ? "6500ms" : "0ms" }}
+                >
+                  <li>
+                    <span className="highlight">Section 4</span>: Information
+                    about myself
+                  </li>
+                </Fade>
+              </ul>
+            </div>
+          </Fade>
+        </div>
       </div>
     </div>
   );
